50f718b6f464ebf3f4bbe929c9c2010b2b128090
Marco Illescas <mu.illescasflores@ugto.mx>
2026-02-11 15:04:19 -0600

    Functional backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,8 @@ from app.db import connect_to_mongo, close_mongo_connection
 app = FastAPI(title="TinyURL Clone API")
 
 # Start-up and shutdown events for the DB connection
-app.add_event_handler("startup", connect_to_mongo())
-app.add_event_handler("shutdown", close_mongo_connection())
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
 
 # Record routes
 app.include_router(urls.router, tags=["URLs"])
