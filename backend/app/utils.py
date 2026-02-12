596f44830cafd3b012d9c97966ffd3b4f3f198bb
Marco Illescas <mu.illescasflores@ugto.mx>
2026-02-12 08:55:00 -0600

    Update utils.py Hashing algorithm.

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,7 +1,12 @@
-import string
-import secrets
+import hashlib
+import base64
 
-def generate_short_id(length: int = 6) -> str:
-	""" Generates a safe, 6-character alphanumeric random string """
-	chars = string.ascii_letters + string.digits
-	return "".join(secrets.choice(chars) for _ in range(length))
+def generate_short_id(url: str, length: int = 6) -> str:
+    """
+    Generates a deterministic hash based on the URL.
+    Uses MD5 to obtain a fingerprint and then encodes it to make it readable.
+    """
+    hash_object = hashlib.md5(url.encode())
+    b64_string = base64.urlsafe_b64encode(hash_object.digest()).decode()
+    
+    return b64_string[:length]
